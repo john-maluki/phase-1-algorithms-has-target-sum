2820b040ffed8596c468aea49b5a72204fa5e43e
john <johnmaluki58@gmail.com>
2023-06-28 17:22:56 +0300

    Complete assingment on Has Target Sum Lab

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  It takes O(n2) === quadratic
 */
 
 /* 
   Add your pseudocode here
+  pick the first element and find the difference from target
+  Loop through all numbers and check if the number exist
+  Pick next element and repeat line above
+
 */
 
 /*
   Add written explanation of your solution here
+  Given a target number and array,  return true if two elements add up to that target. 
+  Otherwise false
 */
 
 // You can run `node index.js` to view these console logs
